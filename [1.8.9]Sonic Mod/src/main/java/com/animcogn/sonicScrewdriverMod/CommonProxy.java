6eb725156c842f6bd66c1122da847de34ddfdeec
Animcogn <corbanvilla@gmail.com>
2016-02-28 17:47:44 -0700

    Built Forge and Transfered Workspace

---
+++ [1.8.9]Sonic Mod/src/main/java/com/animcogn/sonicScrewdriverMod/CommonProxy.java
@@ -0,0 +1,22 @@
+package com.animcogn.sonicScrewdriverMod;
+
+import com.animcogn.sonicScrewdriverMod.world.WorldGen;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class CommonProxy {
+
+	public void preInit(FMLPreInitializationEvent e) {
+			
+    }
+
+    public void init(FMLInitializationEvent e) {
+    }
+
+    public void postInit(FMLPostInitializationEvent e) {
+
+    }
+}
---
+++ [1.8.9]Sonic Mod/src/main/java/com/animcogn/sonicScrewdriverMod/SonicAxe.java
@@ -0,0 +1,15 @@
+package com.animcogn.sonicScrewdriverMod;
+
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemAxe;
+
+public class SonicAxe extends ItemAxe{
+
+	protected SonicAxe(ToolMaterial material) {
+		super(material);
+		this.setUnlocalizedName("sonicAxe");
+	}
+
+	
+	
+}
